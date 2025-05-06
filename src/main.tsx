f0c449c67bdfefd43bf74e06f01603bd02f7011c
Gabriel Lima <124406585+gbrljlima@users.noreply.github.com>
2025-05-06 19:09:05 -0300

    Instalação React Vite TS

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/home'
+
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <>
+            <h1>Ecoviva FrontEnd</h1>
+        </>
+    )
+}
+
+export default Home;
